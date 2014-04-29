ec7f82a76e9269401b8193535628913a952bdd84
tedy <tadeus@gmail.com>
2014-04-29 16:32:58 -0300

    change target database

--- ar.coop.arena.security.server/src/ar/coop/arena/security/server/services/common/sql/DerbySqlService.java
+++ ar.coop.arena.security.server/src/ar/coop/arena/security/server/services/common/sql/DerbySqlService.java
@@ -7,8 +7,24 @@ public class DerbySqlService extends AbstractDerbySqlService implements IService
 
   @Override
   protected String getConfiguredJdbcMappingName() {
+    /*try {
+      File currentDirectory = new File(new File(".").getAbsolutePath());
+      System.out.println(currentDirectory.getCanonicalPath());
+      System.out.println(currentDirectory.getAbsolutePath());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }*/
+
+    // TODO Ver de sacarlo y ponerlo en 
+    /*URL location = DerbySqlService.class.getProtectionDomain().getCodeSource().getLocation();
+    System.out.println(location.getFile());
+    String dirLocation = location.getFile();
+    //    String dirLocation = "/home/work/projects/arena/security/wks-scout-security/";
+
+    return "jdbc:derby:" + dirLocation + "../db/security";*/
 //    return "jdbc:derby:/home/work/projects/arena/security/wks-scout-security-V2/db/security";
 //    return "jdbc:derby:security/dbSecurity;create=true";
+    //Apunta al /home@user
     return "jdbc:derby:security/dbSecurity";
   }
 }
