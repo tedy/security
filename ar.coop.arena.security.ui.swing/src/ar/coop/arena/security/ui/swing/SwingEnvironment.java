5e225586e72c039df518d7d3441b9d544be90e99
tedy <tadeus@gmail.com>
2014-05-07 20:22:08 -0300

    init personalize desktop

--- ar.coop.arena.security.ui.swing/src/ar/coop/arena/security/ui/swing/SwingEnvironment.java
+++ ar.coop.arena.security.ui.swing/src/ar/coop/arena/security/ui/swing/SwingEnvironment.java
@@ -4,4 +4,11 @@ import org.eclipse.scout.rt.ui.swing.DefaultSwingEnvironment;
 
 public class SwingEnvironment extends DefaultSwingEnvironment {
 
+  /*@Override
+  public ISwingScoutRootFrame createRootComposite(Frame rootframe, IDesktop desktop) {
+    ISwingScoutRootFrame ui = new LegacySwingScoutRootFrame(this, rootframe, desktop);
+    decorate(desktop, ui);
+    return ui;
+  }*/
+
 }
