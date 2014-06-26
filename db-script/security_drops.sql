3c8a680f4d8d7a0e1ae1c10f926e7417e2f2ac54
tedy <tadeus@gmail.com>
2014-06-25 23:38:22 -0300

    Add drops tables

---
+++ db-script/security_drops.sql
@@ -0,0 +1,13 @@
+drop table TARGETITEM;
+drop table TARGET;
+drop table TARGETTYPE;
+
+drop table RISK;
+drop table ICONS;
+
+drop table PROJECTTOOL;
+drop table TOOL;
+drop table PROJECT;
+
+drop table FRAMEWORKITEM; 
+drop table FRAMEWORK;
